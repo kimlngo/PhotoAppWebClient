4613944717e9f806f51a4776e874a8cb4d9f9de7
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-12-20 04:53:11 -0500

    Clean up & Format

--- src/main/java/com/kimlngo/ws/clients/controller/AlbumsController.java
+++ src/main/java/com/kimlngo/ws/clients/controller/AlbumsController.java
@@ -3,21 +3,12 @@ package com.kimlngo.ws.clients.controller;
 import com.kimlngo.ws.clients.response.AlbumRest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
@@ -40,11 +31,11 @@ public class AlbumsController {
         String url = "http://localhost:8082/albums";
 
         List<AlbumRest> albumRests = webClient.get()
-                .uri(url)
-                .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<List<AlbumRest>>() {
-                })
-                .block();//ok for now
+                                              .uri(url)
+                                              .retrieve()
+                                              .bodyToMono(new ParameterizedTypeReference<List<AlbumRest>>() {
+                                              })
+                                              .block();//ok for now
 
         model.addAttribute("albums", albumRests);
 
