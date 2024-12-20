243e0427086cbda47d57fc1ca4bab03f65dc7163
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-12-19 22:31:26 -0500

    Configure OAuth2 Client and Provider

--- src/main/java/com/kimlngo/ws/clients/controller/AlbumsController.java
+++ src/main/java/com/kimlngo/ws/clients/controller/AlbumsController.java
@@ -1,6 +1,11 @@
 package com.kimlngo.ws.clients.controller;
 
 import com.kimlngo.ws.clients.response.AlbumRest;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +14,14 @@ import java.util.Arrays;
 
 @Controller
 public class AlbumsController {
-
     @GetMapping("/albums")
-    public String getAlbums(Model model) {
+    public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+        System.out.println("Principal = " + principal);
+
+        var idToken = principal.getIdToken();
+        var idTokenValue = idToken.getTokenValue();
+        System.out.println("idTokenValue = " + idTokenValue);
+
         AlbumRest albumOne = new AlbumRest();
         albumOne.setAlbumId("albumOne");
         albumOne.setAlbumTitle("Album One Title");
