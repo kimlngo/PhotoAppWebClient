71655fce523f4a2bbca8601c3c5dc61580c04d23
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-12-20 03:59:44 -0500

    Reading JWT Access Token

--- src/main/java/com/kimlngo/ws/clients/controller/AlbumsController.java
+++ src/main/java/com/kimlngo/ws/clients/controller/AlbumsController.java
@@ -4,7 +4,13 @@ import com.kimlngo.ws.clients.response.AlbumRest;
 import org.apache.commons.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,8 +20,19 @@ import java.util.Arrays;
 
 @Controller
 public class AlbumsController {
+
+    @Autowired
+    private OAuth2AuthorizedClientService oauth2ClientService;
+
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+
+        OAuth2AuthorizedClient authorizedClient = oauth2ClientService.loadAuthorizedClient(oauthToken.getAuthorizedClientRegistrationId(), oauthToken.getName());
+        String tokenValue = authorizedClient.getAccessToken().getTokenValue();
+        System.out.println("JWT Access Token: " + tokenValue);
+
         System.out.println("Principal = " + principal);
 
         var idToken = principal.getIdToken();
